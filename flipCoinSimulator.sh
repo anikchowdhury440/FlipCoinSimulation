611724b4770133622b558c27a1264891587e86a7
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-09-04 14:08:42 +0530

    If tie the game continues till the difference of minimum 2 points achieved

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -7,6 +7,19 @@ function getCoinFlipResult() {
         echo $(( RANDOM%2 ))
 }
 
+function getWinner() {
+        head_count=$1
+        tail_count=$2
+        if [ $head_count -gt $tail_count ]
+        then
+                difference=$(( $head_count-$tail_count ))
+                echo "Head Wins! Head wins $difference times more than Tail"
+        else
+                difference=$(( $tail_count-$head_count ))
+                echo "Tail Wins! Tail Wins $difference times more than Head"
+        fi
+}
+
 while [[ $count_head -lt 21 && $count_tail -lt 21 ]]
 do
         coin_flip_result="$( getCoinFlipResult )"
@@ -21,14 +34,20 @@ done
 if [ $count_head -ne $count_tail ]
 then
         echo "It's a win"
-        if [ $count_head -gt $count_tail ]
-        then
-                difference=$(( $count_head-$count_tail ))
-                echo "Head Wins! Head wins $difference times more than Tail"
-        else
-                difference=$(( $count_tail-$count_head ))
-                echo "Tail Wins! Tail Wins $difference times more than Head"
-        fi
+	getWinner $count_head $count_tail
 else
         echo "It's a tie"
+	echo "Tie Breaker starts Head or Tail wins if there is a difference of minimum 2 is achieved"
+        while [[ $(( $count_head-$count_tail )) -ne 2 && $(( $count_tail-$count_head )) -ne 2 ]]
+        do
+                coin_flip_result="$( getCoinFlipResult )"
+                if [[ $coin_flip_result -eq 0 ]]
+                then
+                        ((count_head++))
+                else
+			((count_tail++))
+                fi
+        done
+        getWinner $count_head $count_tail
 fi
+
