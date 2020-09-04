01aa45ceb2efbcc7baf31e013167ddedb1b20575
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-09-04 13:48:36 +0530

    Loop multiple times of flipping coins and show number of wins for head and tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -x
 
-coin_flip_result=$(( RANDOM%2 ))
+count_head=0
+count_tail=0
 
-if [[ $coin_flip_result -eq 0 ]]
-then
-        echo "Head"
-else
-        echo "Tail"
-fi
+function getCoinFlipResult() {
+        echo $(( RANDOM%2 ))
+}
+
+for (( i=0;i<10;i++ ))
+do
+        coin_flip_result="$( getCoinFlipResult )"
+	if [[ $coin_flip_result -eq 0 ]]
+	then
+		((count_head++))
+	else
+		((count_tail++))
+	fi
+done
+
+echo "Number of times Head won: " $count_head
+echo "Number of times Tail won: " $count_tail
