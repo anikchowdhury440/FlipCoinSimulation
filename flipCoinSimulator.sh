6ba88c95af8b979f368d140f91accd98db1b9b0d
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-09-07 10:37:53 +0530

    continue until head or tail win 21 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,19 +3,26 @@
 count_head=0
 count_tail=0
 
+read -p "Enter the number of times do you want to flip: " n
+
 function getCoinFlipResult() {
         echo $(( RANDOM%2 ))
 }
 
-while [[ $count_head -lt 21 && $count_tail -lt 21 ]]
+for (( i=1;i<=n;i++ ))
 do
         coin_flip_result="$( getCoinFlipResult )"
-	if [[ $coin_flip_result -eq 0 ]]
-	then
-		((count_head++))
-	else
-		((count_tail++))
-	fi
+
+        if [[ $coin_flip_result -eq 0 ]]
+        then
+                ((count_head++))
+        else
+                ((count_tail++))
+        fi
+        if [[ ( $count_head -eq 21 || $count_tail -eq 21 ) ]]
+        then
+                break
+        fi
 done
 
 if [ $count_head -ne $count_tail ]
@@ -23,12 +30,18 @@ then
         echo "It's a win"
         if [ $count_head -gt $count_tail ]
         then
+		echo "Head Count: " $count_head
+        	echo "Tail Count: " $count_tail
                 difference=$(( $count_head-$count_tail ))
                 echo "Head Wins! Head wins $difference times more than Tail"
         else
+		echo "Head Count: " $count_head
+        	echo "Tail Count: " $count_tail
                 difference=$(( $count_tail-$count_head ))
                 echo "Tail Wins! Tail Wins $difference times more than Head"
         fi
 else
         echo "It's a tie"
+	echo "Head Count: " $count_head
+        echo "Tail Count: " $count_tail
 fi
